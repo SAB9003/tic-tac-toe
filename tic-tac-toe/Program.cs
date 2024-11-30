b835c362bfc3cc2759531538b41c141577f51e13
Alex9003 <agl.luchko@gmail.com>
2024-11-30 23:16:00 +0200

    Delite else in switch

--- tic-tac-toe/Program.cs
+++ tic-tac-toe/Program.cs
@@ -4,7 +4,7 @@ Console.WriteLine("Yoy have opened the tic-tac-toe program");
 char first_field = '1', second_field = '2', third_field = '3',
     fourth_field = '4', fifth_field = '5', sixth_field = '6',
     seventh_field = '7', eighth_field = '8', ninth_field = '9',
-    player = 'X', choose_field;
+    player = 'X', choose_field, selected_field =' ';
 bool not_game_over = true, who_won;
 byte number_of_moves = 1,last_move=9;
 while (number_of_moves <= last_move && not_game_over)
@@ -24,108 +24,80 @@ while (number_of_moves <= last_move && not_game_over)
     Console.WriteLine("");
     switch (choose_field)
     {
+        case '1': 
+            selected_field = first_field; break;
+        case '2': 
+            selected_field = second_field; break;
+        case '3': 
+            selected_field = third_field; break;
+        case '4': 
+            selected_field = fourth_field; break;
+        case '5': 
+            selected_field = fifth_field; break;
+        case '6': 
+            selected_field = sixth_field; break;
+        case '7': 
+            selected_field = seventh_field; break;
+        case '8': 
+            selected_field = eighth_field; break;
+        case '9': 
+            selected_field = ninth_field; break;
+        default:
+            Console.WriteLine("Field entered incorrectly");
+            continue;
+    }
+    if (selected_field == 'X' || selected_field == 'O')
+    {
+        Console.WriteLine("This field is already taken");
+        continue;
+    }
+    switch (choose_field)
+    {
         case '1':
             {
-                if (first_field == '1')
-                    first_field = player;
-                else
-                {
-                    Console.WriteLine("This field is already taken");
-                    number_of_moves--;
-                }
-                break;
+            first_field = player;
+            break;
             }
         case '2':
             {
-                if (second_field == '2')
-                    second_field = player;
-                else
-                {
-                    Console.WriteLine("This field is already taken");
-                    number_of_moves--;
-                }
-                break;
+            second_field = player;
+            break;
             }
         case '3':
             {
-                if (third_field == '3')
-                    third_field = player;
-                else
-                {
-                    Console.WriteLine("This field is already taken");
-                    number_of_moves--;
-                }
-                break;
+            third_field = player;
+            break;
             }
         case '4':
             {
-                if (fourth_field == '4')
-                    fourth_field = player;
-                else
-                {
-                    Console.WriteLine("This field is already taken");
-                    number_of_moves--;
-                }
-                break;
+            fourth_field = player;
+            break;
             }
         case '5':
             {
-                if (fifth_field == '5')
-                    fifth_field = player;
-                else
-                {
-                    Console.WriteLine("This field is already taken");
-                    number_of_moves--;
-                }
-                break;
+            fifth_field = player;
+            break;
             }
         case '6':
             {
-                if (sixth_field == '6')
-                    sixth_field = player;
-                else
-                {
-                    Console.WriteLine("This field is already taken");
-                    number_of_moves--;
-                }
-                break;
+            sixth_field = player;
+            break;
             }
         case '7':
             {
-                if (seventh_field == '7')
-                    seventh_field = player;
-                else
-                {
-                    Console.WriteLine("This field is already taken");
-                    number_of_moves--;
-                }
-                break;
+            seventh_field = player;
+            break;
             }
         case '8':
             {
-                if (eighth_field == '8')
-                    eighth_field = player;
-                else
-                {
-                    Console.WriteLine("This field is already taken");
-                    number_of_moves--;
-                }
-                break;
+            eighth_field = player;
+            break;
             }
         case '9':
             {
-                if (ninth_field == '9')
-                    ninth_field = player;
-                else
-                {
-                    Console.WriteLine("This field is already taken");
-                    number_of_moves--;
-                }
-                break;
-            }
-        default:
-            Console.WriteLine("Fiend entred incorrectly");
+            ninth_field = player;
             break;
+        }
     }
     //Перевірка по горизонталі
     if ((first_field == second_field && second_field == third_field) ||
